e5362b0c6efbe0b5eb63ff075542a36dfdb3a15b
Dimal Kavindu J. <80914328+DimalJay@users.noreply.github.com>
2026-06-18 19:07:36 +0530

    update proxy auth code

--- proxy.ts
+++ proxy.ts
@@ -1,7 +1,7 @@
 import { NextRequest, NextResponse } from "next/server";
 
 export function proxy(request: NextRequest) {
-  const hasToken = request.cookies.has("authx");
+  const hasToken = request.cookies.has("auth_token");
   const pathname = request.nextUrl.pathname;
   const isAuthpage = pathname.startsWith("/login") || pathname.startsWith("/register");
   const publicPage = pathname === '/' || pathname.startsWith("/event");
